3355be0037a3f2411ac4055741dd155f033fd725
Heinz N. Gies <heinz@licenser.net>
2012-07-08 00:52:51 +0200

    Moved to backyard.

--- src/libchunter.erl
+++ src/libchunter.erl
@@ -1,7 +1,7 @@
 %%%-------------------------------------------------------------------
 %%% @author Heinz N. Gies <heinz@licenser.net>
 %%% @copyright (C) 2012, Heinz N. Gies
-%%% @doc
+%%% @doc This module provides an interface to the chunter server.
 %%%
 %%% @end
 %%% Created : 11 May 2012 by Heinz N. Gies <heinz@licenser.net>
@@ -23,36 +23,115 @@
 	 get_dataset/3,
 	 list_keys/2]).
 
+%% @type uuid() = binary().
+-type uuid() ::
+	binary().
+
+%% @type machine() = uuid().
+-type machine() ::
+	uuid().
+
+
+%% @type user() = uuid().
+-type user() :: uuid().
+
+%% @type cached_auth() = {user(), permissions()}.
+-type cached_auth() :: 
+	{user(), permissions()}.
+
+%% @type auth() = system | user() | cached_auth().
+-type auth() ::
+	system | 
+	user() |
+	cached_auth().
+
 %%%===================================================================
 %%% API
 %%%===================================================================
 
 %%--------------------------------------------------------------------
-%% @doc
-%% @spec
+%% @spec (pid(), auth()) -> [machine()]
+%%
+%% @doc Lists all machines on a server visible to the user.
+%%
 %% @end
 %%--------------------------------------------------------------------
 
 list_machines(Pid, Auth) ->
     chunter_call(Pid, Auth, {machines, list}).
 
+%%--------------------------------------------------------------------
+%% @spec (pid(), auth(), machine()) -> [term()]
+%%
+%% @doc Lists all machines on a server visible to the user.
+%%
+%% This command is pretty much equal to vmadm get machine.
+%% @end
+%%--------------------------------------------------------------------
+
 get_machine(Pid, Auth, UUID) ->
     chunter_call(Pid, Auth, {machines, get, UUID}).
+
+%%--------------------------------------------------------------------
+%% @spec (pid(), auth(), machine()) -> [term()]
+%%
+%% @doc Gets the runtime info of a KVM machine.
+%%
+%% This command is pretty much equal to vmadm info machine.
+%% @end
+%%--------------------------------------------------------------------
 
 get_machine_info(Pid, Auth, UUID) ->
     chunter_call(Pid, Auth, {machines, info, UUID}).
 
+%%--------------------------------------------------------------------
+%% @spec (pid(), auth(), machine()) -> ok
+%%
+%% @doc Starts a machine.
+%%
+%% This command is asyncronous.
+%% @end
+%%--------------------------------------------------------------------
+
+
 start_machine(Pid, Auth, UUID) ->
     chunter_cast(Pid, Auth, {machines, start, UUID}).
+
+%%--------------------------------------------------------------------
+%% @spec (pid(), auth(), machine()) -> ok
+%%
+%% @doc Deletes a machine.
+%%
+%% This command is asyncronous.
+%% @end
+%%--------------------------------------------------------------------
 
 delete_machine(Pid, Auth, UUID) ->
     chunter_cast(Pid, Auth, {machines, delete, UUID}).
 
+%%--------------------------------------------------------------------
+%% @spec (pid(), auth(), 
+%%        binary(), binary(), binary(), 
+%%        [{binary(),binary()}], [{binary(),binary()}]) -> machine()
+%%
+%% @doc Creates a new machine.
+%%
+%% @end
+%%--------------------------------------------------------------------
 
 create_machine(Pid, {Auth, _}, Name, PackageUUID, DatasetUUID, Metadata, Tags) ->
     create_machine(Pid, Auth, Name, PackageUUID, DatasetUUID, Metadata, Tags);
 create_machine(Pid, Auth, Name, PackageUUID, DatasetUUID, Metadata, Tags) ->
     gen_server:call(Pid, {call, Auth, {machines, create, Name, PackageUUID, DatasetUUID, Metadata, Tags}},60000).
+
+%%--------------------------------------------------------------------
+%% @spec (pid(), auth(), 
+%%        machine(), binary()) -> ok.
+%%
+%% @doc Starts a KVM virtual machine from a iso image.
+%%
+%% @end
+%%--------------------------------------------------------------------
 
 start_machine(Pid, Auth, UUID, Image) ->
     chunter_cast(Pid, Auth, {machines, start, UUID, Image}).
